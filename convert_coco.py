03ccfdf41399a583144d221550512924a3c8f56b
Abel <127351087+SirBediwal@users.noreply.github.com>
2026-03-06 21:34:20 +0100

    Update convert_coco.py

--- convert_coco.py
+++ convert_coco.py
@@ -40,6 +40,6 @@ def convert_coco_to_yolo(json_path, output_dir):
 
 if __name__ == "__main__":
     # Pfade anpassen, falls sie bei euch anders heissen
-    json_file = "dataset/train/_annotations.coco"
+    json_file = "dataset/train/_annotations.coco.json"
     label_folder = "dataset/train/labels"
     convert_coco_to_yolo(json_file, label_folder)
